1a3d6dc21701c38b76d22fe1392a72109d3455a9
david alejandro izquierdo cafaro <davidbeisbol@hotmail.com.ar>
2024-09-09 19:13:10 -0300

    dbcontext

---
+++ TicketOn.Server/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketOn.Server.Entidades;
+
+namespace TicketOn.Server
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+        public DbSet<Entrada> Entradas { get; set; }
+        public DbSet<Evento> Eventos { get; set; }
+        public DbSet<Genero> Generos { get; set; }
+        public DbSet<Tanda> Tandas { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var cascadeFKs = modelBuilder.Model.GetEntityTypes()
+                .SelectMany(t => t.GetForeignKeys())
+                .Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Cascade);
+
+            foreach (var fk in cascadeFKs)
+            {
+                fk.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
