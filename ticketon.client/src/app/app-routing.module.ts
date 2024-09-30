422e5a9bf0012ca8dfd233bcefa8122c622362e8
david alejandro izquierdo cafaro <davidbeisbol@hotmail.com.ar>
2024-09-30 17:16:58 -0300

    filtro de eventos

--- ticketon.client/src/app/app-routing.module.ts
+++ ticketon.client/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { IndiceGenerosComponent } from './generos/indice-generos/indice-generos.
 import { CrearGeneroComponent } from './generos/crear-genero/crear-genero.component';
 import { CrearEventoComponent } from './eventos/crear-evento/crear-evento.component';
 import { EditarGeneroComponent } from './generos/editar-genero/editar-genero.component';
+import { FiltroEventosComponent } from './eventos/filtro-eventos/filtro-eventos.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'generos', component: IndiceGenerosComponent },
   { path: 'generos/crear', component: CrearGeneroComponent },
   { path: 'eventos/crear', component: CrearEventoComponent },
-  {path: 'generos/editar/:id', component: EditarGeneroComponent},
+  { path: 'generos/editar/:id', component: EditarGeneroComponent },
+  { path: 'eventos/filtrar', component: FiltroEventosComponent},
   { path: '**', redirectTo: '' }
 ];
 
---
+++ ticketon.client/src/app/eventos/filtro-eventos/filtro-eventos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filtro-eventos',
+  standalone: true,
+  imports:[],
+  templateUrl: './filtro-eventos.component.html',
+  styleUrl: './filtro-eventos.component.css'
+})
+export class FiltroEventosComponent {
+
+}
