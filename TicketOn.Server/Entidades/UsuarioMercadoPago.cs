c34d36ba69dfa2f6827c606c06af1ff3935cd452
Joaquin <joacozamoracab@gmail.com>
2024-12-11 13:29:49 -0300

    prueba mp 40

--- TicketOn.Server/Entidades/UsuarioMercadoPago.cs
+++ TicketOn.Server/Entidades/UsuarioMercadoPago.cs
@@ -5,7 +5,7 @@ namespace TicketOn.Server.Entidades
     public class UsuarioMercadoPago
     {
         public int Id { get; set; }
-        public string UsuarioId { get; set; }  // Relación con el usuario
+        public string? UsuarioId { get; set; }  // Relación con el usuario
         public string AccessToken { get; set; }
         public string RefreshToken { get; set; }
         public DateTime FechaExpiracion { get; set; }
